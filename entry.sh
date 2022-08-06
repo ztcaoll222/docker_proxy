69f8c8dc2e6153ef0a0afa27bf03586282fb3dc2
ztcaoll222 <ztcaoll222@outlook.com>
2022-08-07 02:36:10 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -p /root/.ssh
+ssh-keygen -t rsa -P "" -f /etc/ssh/ssh_host_rsa_key
+ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key
+ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key
 echo ${authorized_keys} >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh -R
 echo -e "Port 22\n" >> /etc/ssh/sshd_config
