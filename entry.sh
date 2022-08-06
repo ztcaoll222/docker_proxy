ef1a00fad608b16ae4995813b7c1e78e2a44c337
ztcaoll222 <ztcaoll222@outlook.com>
2022-08-07 03:06:37 +0800

    push

--- entry.sh
+++ entry.sh
@@ -4,6 +4,7 @@ mkdir -p /root/.ssh
 ssh-keygen -t rsa -P "" -f /etc/ssh/ssh_host_rsa_key
 ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key
 ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key
+echo -e "PasswordAuthentication no" >> /etc/ssh/sshd_config
 echo ${authorized_keys} >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh -R
 /usr/sbin/sshd -p 22 -D
