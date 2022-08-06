a87dcf3c4c68e423ef6ac567cd8b42930fbb786a
ztcaoll222 <ztcaoll222@outlook.com>
2022-08-07 02:45:07 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -6,6 +6,6 @@ ssh-keygen -t ecdsa -P "" -f /etc/ssh/ssh_host_ecdsa_key
 ssh-keygen -t ed25519 -P "" -f /etc/ssh/ssh_host_ed25519_key
 echo ${authorized_keys} >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh -R
-echo -e "Port 22\n" >> /etc/ssh/sshd_config
 echo -e "PermitRootLogin yes\n" >> /etc/ssh/sshd_config
+/usr/sbin/sshd -D -p 22
 top
