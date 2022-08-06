7f635b2ae1f642456f431490967ab547cf07c13b
ztcaoll222 <ztcaoll222@outlook.com>
2022-08-07 02:03:44 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -3,4 +3,6 @@
 mkdir -p /root/.ssh
 echo ${authorized_keys} >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh -R
+echo -e "Port 22\n" >> /etc/ssh/sshd_config
+echo -e "PermitRootLogin yes\n" >> /etc/ssh/sshd_config
 top
